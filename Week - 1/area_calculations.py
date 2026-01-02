7247efa33854d0a24f1fe0159323117e57a0d353
zebunnisa-codes <zaibunnisaap19@gmail.com>
2026-01-02 16:54:51 +0530

    Add files via upload

---
+++ Week - 1/area_calculations.py
@@ -0,0 +1,11 @@
+def area_of_circle(radius):
+    return 3.14 * radius * radius
+
+def area_of_triangle(base, height):
+    return 0.5 * base * height
+
+def area_of_square(side):
+    return side * side
+
+def area_of_rectangle(length, width):
+    return length * width
---
+++ Week - 1/execute.py
@@ -0,0 +1,27 @@
+from area_calculations import *
+print("Program begins....... ", "\n")
+
+#Circle
+print("To calculate area of circle.........")
+r = float(input("Enter radius of circle : "))
+print("Area of circle is ", area_of_circle(r), "\n")
+
+#Triangle 
+print("To calculate area of triangle.........")
+b=float(input("Enter base of triangle : "))
+h=float(input("Enter height of triangle : "))
+print("Area of Triangle is ", area_of_triangle(b, h), "\n")
+
+#Square
+print("To calculate area of square.........")
+s = float(input("Enter side of square : " ))
+print("Area of Square is ", area_of_square(s), "\n")
+
+#Rectangle
+print("To calculate area of rectangle.........")
+l = float(input("Enter length of rectangle : " ))
+w = float(input("Enter width of rectangle : " ))
+print("Area of Rectangle is ", area_of_rectangle(l, w), "\n")
+
+print("Program ends....... ", "\n")
+
